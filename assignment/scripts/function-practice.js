d4507d0821ae7b1a747f4b3827eb4cccf5d3b86f
Jade Wiegel <jadewiegel@gmail.com>
2022-10-23 12:46:43 -0400

    completed up to addition.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -10,21 +10,26 @@ function hello() {
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
+//--------
 
-
-// 2. Function to return an personalized hello, using the `name` argument.
+// 2. Function to return a personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return 'Hello Brooke!';
 }
 // Remember to call the function to test
-
+console.log('Test personalized hello', helloName());
+//-------
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
+  console.log('First Number:', firstNumber, 'Second Number:', secondNumber);
+  let answer = firstNumber + secondNumber;
+  return answer;
   // return firstNumber + secondNumber;
 }
-
+console.log('First number + Second number:', addNumbers(2,4));
+//-------
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( ){
